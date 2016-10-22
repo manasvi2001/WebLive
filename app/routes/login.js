0038414145ba9bce18e1304bfc3b7a76a73fd9b8
ved <ved.mulkalwar@gmail.com>
2016-10-22 14:19:04 +0530

    added flock dependencies

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,4 +2,8 @@ module.exports = function (app) {
     app.get('/logintest',function (req, res) {
         res.json({success:true});
     })
+
+    app.get('/install',function(req,res){
+
+    })
 }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
+var port     = process.env.PORT || 9000;
 var mongoose = require('mongoose');
 
 var cookieParser = require('cookie-parser');
@@ -22,6 +22,30 @@ mongoose.connect(configDB.db); // connect to our database
 app.use(bodyParser()); // get information from html forms
 
 app.set('view engine', 'ejs'); // set up ejs for templating
+
+//flock credentials
+var flock = require('flockos');
+
+flock.setAppId('d910233d-b87b-4648-b41d-bccdc4240b84');
+flock.setAppSecret('0e2d9834-7336-451a-80f9-7962f6306ee0');
+
+//To verify event tokens, you can either use the verifyEventToken function (this only works if have set the app secret):
+//flock.verifyEventToken(token);
+
+//middleware to verify tokens
+//app.use(flock.eventTokenChecker);
+
+app.post('/events', flock.router);
+
+flock.events.on('app.install', function (event) {
+    console.log("app.install called with->",JSON.stringify(event));
+    return {
+        success:true
+    }
+});
+
+//
+
 
 // routes ======================================================================
 require('./app/routes/login.js')(app); // load our routes and pass in our app and fully configured passport
